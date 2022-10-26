ab9d3d57af16d0e384039a58364d2f7bcd2827d8
muthub <muthub@rbos.co.uk>
2022-10-26 23:49:50 +0530

    26-10-22

--- src/main/java/com/example/demo/StudentController.java
+++ src/main/java/com/example/demo/StudentController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class StudentController {
 	@GetMapping("/")
 	public String sayHello() {
-		return "Hello World..";
+		return "Welcome...";
 	}
 
 }
